6d26c1d3082dc212a36d4e8840b7f8b978190e72
reki9185 <decimo9185@gmail.com>
2024-10-01 21:00:16 +0800

    fix: resolve gci issue

--- internal/sbi/api_hi.go
+++ internal/sbi/api_hi.go
@@ -2,22 +2,23 @@ package sbi
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func (s *Server) getHiRoute() []Route {
-	return []Route {
+	return []Route{
 		{
-			Name: "sayHello",
-			Method: http.MethodGet,
+			Name:    "sayHello",
+			Method:  http.MethodGet,
 			Pattern: "/",
 			APIFunc: s.SayHello,
 			// Use
 			// curl -X GET http://127.0.0.163:8000/hi/ -w "\n"
 		},
 		{
-			Name: "sayGoodBye",
-			Method: http.MethodPost,
+			Name:    "sayGoodBye",
+			Method:  http.MethodPost,
 			Pattern: "/",
 			APIFunc: s.SayGoodBye,
 			// Use
