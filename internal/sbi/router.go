94f20fdccb6d786acf5ef698cac9eb104624ecf4
senselessDog <dragonisflying911@gmail.com>
2024-11-14 11:47:39 +0800

    fix: golangci-lint try 2

--- internal/sbi/router.go
+++ internal/sbi/router.go
@@ -54,7 +54,6 @@ func newRouter(s *Server) *gin.Engine {
 	kuroumiGroup := router.Group("/kuromi")
 	applyRoutes(kuroumiGroup, s.getKuromiRoute())
 
-	// 添加新的路由組
 	newServiceGroup := router.Group("/newservice")
 	applyRoutes(newServiceGroup, s.getNewServiceRoute())
 	return router
